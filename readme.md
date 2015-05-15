a4f6e893a010d7885602d08efc1b235751807b82
sairus2k <sairus2k@gmail.com>
2015-05-15 10:45:38 +0700

    Footer markup

--- readme.md
+++ readme.md
@@ -1,9 +1,6 @@
-# Web site for travel agency Makalu
-
-[makalu8463.com](http://makalu8463.com)
+# Web site for travel agency [Makalu](http://makalu8463.com)
 Based on free PSD template [bluebox](http://www.graphicsfuel.com/2013/07/bluebox-flat-website-psd-templates-design/).
-
 ## Used technologies:
-[WORDPRESS-GULP-BOWER-SASS Starter Kit](https://github.com/synapticism/wordpress-gulp-bower-sass)
-[FontAwesome](http://fontawesome.io/)
-[GoogleFont](https://www.google.com/fonts)
+* [WORDPRESS-GULP-BOWER-SASS Starter Kit](https://github.com/synapticism/wordpress-gulp-bower-sass)
+* [FontAwesome](http://fontawesome.io/)
+* [GoogleFont](https://www.google.com/fonts)
--- src/footer.php
+++ src/footer.php
@@ -1,8 +1,17 @@
       </div>
       <div id="wrap-footer" class="wrap-footer">
         <footer id="colophon" class="site-footer" role="contentinfo">
+         <div class="site-info">
+           <p>&copy; <?php echo date("Y"); ?> Travel Agency &laquo;Makalu&raquo;</p>
+         </div>
           <nav id="site-footer-navigation" role="navigation">
-            <?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'menu-footer', 'menu_class' => 'menu-inline' ) ); ?>
+            <?php wp_nav_menu( array( 
+  'theme_location'  => 'footer',
+  'menu_id'         => 'menu-social',
+  'menu_class'      => 'menu-social',
+  'link_before'     => '<span class="screen-reader-text">',
+  'link_after'      => '</span>',
+) ); ?>
           </nav>
         </footer>
       </div>
@@ -10,3 +19,5 @@
   <?php wp_footer(); ?>
   </body>
 </html>
+
+
